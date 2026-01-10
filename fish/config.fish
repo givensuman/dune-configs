a1119dabb477fa51a150ee3f2da7f18d17672236
givensuman <givensuman@duck.com>
2026-01-10 12:56:57 -0500

    20260110-125657

--- fish/config.fish
+++ fish/config.fish
@@ -13,9 +13,9 @@ set --global fish_prompt_pwd_dir_length 999
 set --global hydro_multiline true
 
 if set -q DISTROBOX_ENTER_PATH
-    set --global hydro_symbol_prompt " [distrobox] "
+    set --global hydro_symbol_prompt "󰇥 [distrobox] "
 else
-    set --global hydro_symbol_prompt " "
+    set --global hydro_symbol_prompt "󰇥 "
 end
 
 set --global hydro_symbol_start "\n"
@@ -24,9 +24,9 @@ set --global hydro_symbol_git_ahead "  "
 set --global hydro_symbol_git_behind "  "
 
 # catppuccin
-set --global hydro_color_prompt 89b4fa
+set --global hydro_color_prompt f9e2af
 set --global hydro_color_error f38ba8
-set --global hydro_color_pwd b4befe
+set --global hydro_color_pwd 89b4fa
 set --global hydro_color_git f9e2af
 set --global hydro_color_duration 94e2d5
 
